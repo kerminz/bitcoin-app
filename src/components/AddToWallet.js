e196ea92f283848fcf35f06b66748fbd4d13c0fe
Kermin Zahirovic <kermin@Kermins-iMac.fritz.box>
2020-04-26 14:36:14 +0200

    input validation for wallet field

--- src/components/AddToWallet.js
+++ src/components/AddToWallet.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withFormik } from 'formik';
 import * as Yup from 'yup';
-import { Button, Drawer } from 'antd';
+import { Button, Drawer, Divider } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { Form, Input } from "formik-antd";
 import Notification from './Notification'
@@ -21,22 +21,27 @@ class AddToWallet extends React.Component {
 
     handleClose = (e) => {
         this.setState({ visible: false })
-        if (e.target.name === "save" && this.props.values.wallet.length !== 0) {
-            Notification('Amount has been added successfully', `BTC Amount: "${this.props.values.wallet}" has been saved successfully.`, 'success')
-        }
     }
 
-    handleChange = (e, { value }) => this.setState({ domain: value })
+    handleSave = (e) => {
+        if (e.target.name === "save" && this.props.values.wallet.length !== 0 && !this.props.errors.wallet) {
+            const walletValue = parseFloat(this.props.values.wallet.replace(',', '.').replace(' ', ''))
+            console.log(walletValue)
+            Notification('Amount has been added successfully', `BTC Amount: "${this.props.values.wallet}" has been saved successfully.`, 'success')
+            this.setState({ visible: false })
+            this.props.values.wallet = ""
+        }
+    }
 
     render() {
         return (
             <div>
                 <Button type="primary" size="large" onClick={this.handleOpen}>
-                    <PlusOutlined /> Add BTC
+                    <PlusOutlined /> Add Bitcoins
                 </Button>
                 <Drawer
                     title="Add Bitcoins"
-                    width="30vw"
+                    width="40vw"
                     onClose={this.handleClose}
                     visible={this.state.visible}
                     bodyStyle={{ paddingBottom: 80 }}
@@ -51,22 +56,12 @@ class AddToWallet extends React.Component {
                             />
                         </Form.Item>
                     </Form>
-                    <div
-                        style={{
-                            position: 'absolute',
-                            right: 0,
-                            bottom: 0,
-                            width: '100%',
-                            borderTop: '1px solid #e9e9e9',
-                            padding: '10px 16px',
-                            background: '#fff',
-                            textAlign: 'right',
-                        }}
-                    >
-                        <Button name="cancel" onClick={this.handleClose} style={{ marginRight: 8 }}>
+                    <Divider />
+                    <div style={{ position: "absolute", right: "0", marginRight: "1rem" }}>
+                        <Button name="cancel" size="large" onClick={this.handleClose} style={{ marginRight: 8 }}>
                             Cancel
                         </Button>
-                        <Button name="save" onClick={this.handleClose} type="primary">
+                        <Button name="save" size="large" onClick={this.handleSave} type="primary">
                             Add to wallet
                          </Button>
                     </div>
@@ -91,7 +86,11 @@ const mapPropsToState = (state) => {
 const formik = withFormik({
     mapPropsToValues,
     validationSchema: Yup.object().shape({
-        wallet: Yup.string().max(250).required('Bitcoin amount is required.')
+        wallet: Yup.string().test(
+            'is-decimal',
+            'invalid decimal',
+            value => (value + "").match(/^[0-9]+(\,[0-9]{1,2})?$/),
+        ).required('Bitcoin amount is required.')
     }),
     handleSubmit: () => {
 
